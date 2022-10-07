c81656e767ca17c2a2ab1bbfa0e87575d14fdd8e
Şafak <omersafakbebek@gmail.com>
2022-10-07 21:36:18 +0300

    Initalization of QuizApp project with Node.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const mongoose =require('mongoose');
+
+require('dotenv/config');
+const url = process.env.DB_CONNECTION;
+
+app.get('/',(req,res)=>{
+    res.send('We are on home');
+});
+mongoose.connect(url, {useNewUrlParser: true}, ()=>
+    console.log('connected to db',url)
+);
+
+
+app.listen(8090);
