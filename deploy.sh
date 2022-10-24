6d6ec808f468621d7f04c19c637963e8375c9528
Şafak <omersafakbebek@gmail.com>
2022-10-24 16:57:59 +0300

    deleted .env

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync --rsync-path="sudo rsync" -r -avh --exclude={"deploy.sh","index.html","logo.png"} /home/${USER}/quizapp_node ubuntu@ec2-54-164-128-198.compute-1.amazonaws.com:/opt/projects/ --delete
+rsync --rsync-path="sudo rsync" -r -avh --exclude={"deploy.sh","index.html","logo.png",".git"} /home/${USER}/quizapp_node ubuntu@ec2-54-164-128-198.compute-1.amazonaws.com:/opt/projects/ --delete
 ssh ubuntu@ec2-54-164-128-198.compute-1.amazonaws.com sudo systemctl restart quizapp_node.service
